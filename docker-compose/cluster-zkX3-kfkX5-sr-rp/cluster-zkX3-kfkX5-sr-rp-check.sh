1daf7874532cb6a7a05a978d44faf79696b8fd52
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2017-11-10 18:00:41 +0100

    Updated checker scripts

--- docker-compose/cluster-zkX3-kfkX5-sr-rp/cluster-zkX3-kfkX5-sr-rp-check.sh
+++ docker-compose/cluster-zkX3-kfkX5-sr-rp/cluster-zkX3-kfkX5-sr-rp-check.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 for i in 12181 22181 32181; do
-  echo "ZooKeeper instance listening on port $i is in"
+  echo "ZooKeeper instance listening on port $i:"
+  docker run --net=host --rm mbe1224/confluent-zookeeper bash -c "echo ruok | nc localhost $i"
+  echo ""
   docker run --net=host --rm mbe1224/confluent-zookeeper bash -c "echo stat | nc localhost $i | grep Mode"
   echo "---------------------------------------------"
 done
