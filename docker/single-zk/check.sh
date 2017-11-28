3ed6140891d4d6e77d4b3412db42ca4ef972e18d
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2017-11-28 11:26:12 +0100

    Simplified sripts, default ports

--- docker/single-zk/check.sh
+++ docker/single-zk/check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in 12181; do
+for i in 2181; do
   echo "ZooKeeper instance listening on port $i:"
   docker run --net=host --rm mbe1224/confluent-zookeeper bash -c "echo ruok | nc localhost $i"
   echo ""
